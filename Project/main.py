90ab716c58ae6b6b40b3bbe1d1b7fa1cd1860da1
abdullahw1 <90009221+abdullahw1@users.noreply.github.com>
2024-04-03 14:40:21 -0700

    db post with endpoint

--- Project/main.py
+++ Project/main.py
@@ -12,7 +12,7 @@ import requests
 with open('encodings.pkl', 'rb') as f:
     data = pickle.load(f)
 encodeListKnownFaces = data['encodings']
-classNames = data['labels']
+# classNames = data['labels']
 
 # Function to fetch student records from the database
 def fetch_students():
@@ -25,7 +25,7 @@ def fetch_students():
             password="16ae*c2cbg4Dc*eGEfGCg33g11Fb4gDB"
         )
         cur = conn.cursor()
-        cur.execute("SELECT studentid, photo_url FROM students")
+        cur.execute("SELECT studentid, first_name, last_name FROM students")  # Selecting first_name and last_name columns
         return cur.fetchall()
     except Exception as e:
         print(f'Error: {e}')
@@ -35,30 +35,14 @@ def fetch_students():
 
 # Fetch student records from the database
 students = fetch_students()
+# Get the names of the students from the fetched records
+classNames = [f"{student[1]} {student[2]}" for student in students]  # Combining first and last names
+
+# # Get the names of the students from the fetched records
+# classNames = [student[1] for student in students]  # Assuming the name is stored in the second column of the fetched records
 
 # Get current timestamp in the correct format
 timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-# Function to connect to the PostgreSQL database and insert attendance data
-# def markAttendanceOnDatabase(attendance_id, class_id, student_id, timestamp):
-#     try:
-#         conn = psycopg2.connect(
-#             host="monorail.proxy.rlwy.net",
-#             port="28291",
-#             dbname="railway",
-#             user="postgres",
-#             password="16ae*c2cbg4Dc*eGEfGCg33g11Fb4gDB"
-#         )
-#         cur = conn.cursor()
-#         insert_query = "INSERT INTO attendance (attendanceid, classid, studentid, entry_timestamp) VALUES (%s, %s, %s, %s)"
-#         cur.execute(insert_query, (attendance_id, class_id, student_id, timestamp))
-#         conn.commit()
-#         print(f'Success: {timestamp} attendance inserted into the database.')
-#     except Exception as e:
-#         print(f'Error: {e}')
-#     finally:
-#         cur.close()
-#         conn.close()
 
 # Function to mark attendance in CSV file
 def markAttendanceInCSV(attendance_id, class_id, student_id, entry_timestamp):
@@ -111,25 +95,14 @@ while True:
 
             # Call function to mark attendance in the CSV file
             markAttendanceInCSV(attendance_id_counter, class_id, student_id, timestamp)
-            # Uncomment this line to mark attendance in the database
-            #markAttendanceOnDatabase(attendance_id_counter, class_id, student_id, timestamp)
+
             # Send HTTP POST request to the endpoint
-            # url = 'http://localhost:8000/attendance_record'
-            # data = {
-            #     'attendance_id': attendance_id_counter,
-            #     'student_id': student_id,
-            #     'class_id': class_id,
-            #     'entry_timestamp': timestamp
-            # }
-            # response = requests.post(url, json=data)
-            url = f'http://localhost:8000/attendance_record?attendance_id={attendance_id_counter}&student_id={student_id}&class_id={class_id}&timestamp={timestamp}'
+            url = f'http://localhost:8000/attendance_record?attendanceid={attendance_id_counter}&student_id={student_id}&class_id={class_id}&timestamp={timestamp}'
             response = requests.post(url)
             if response.status_code == 200:
                 print("Attendance recorded successfully.")
             else:
-                print("Failed to record attendance. ")
-
-    # Display the webcam feed with recognized
+                print(f"Failed to record attendance. \n attendance_record?attendance_id={attendance_id_counter}&student_id={student_id}&class_id={class_id}&timestamp={timestamp}")
 
     # Display the webcam feed with recognized faces
     cv2.imshow('webcam', img)
@@ -142,6 +115,3 @@ while True:
 # Release the webcam and close all windows
 cap.release()
 cv2.destroyAllWindows()
-
-
-
